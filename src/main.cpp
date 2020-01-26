3d555c723a2c4b3374311db3f9c97329f7e2efe3
eaglx <eaglx.programmer@gmail.com>
2020-01-26 14:08:34 +0100

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
-#include <iostream>
+#include "../include/main.hpp"
+#include "../include/vmcpu.hpp"
 
 int main()
 {
     std::cout << "***************" << std::endl;
     std::cout << "VMPROTECT v0.01" << std::endl;
     std::cout << "***************" << std::endl;
+
+    VMCPU *vm = new VMCPU();
+
+    BYTE *code = loadProtectedCode();
+    std::cout << "[DEBUG] " << unsigned(code[1]) << std::endl;
+    vm->loadCode(code);
+    deleteProtectedCode(code);
+    delete vm;
 
     return 0;
 }
